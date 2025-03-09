0771721126d9a4c52beebe04f6c6ec9a6f99b3e3
ntwarifrank <ntwarifrank100@gmail.com>
2025-03-09 14:50:26 +0200

    last cors prob

--- index.js
+++ index.js
@@ -41,19 +41,6 @@ cloudinary.config({
 //const upload = multer({ storage });
 
 const upload = multer({ dest: "/uploads" });
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["https://zipbuy.vercel.app", "https://zipbuy-admin.vercel.app"]);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 const corsOptions = {
   origin: [
